45d69e52da7bea756e8d8513fae81779a326c0c0
Abraham R Burnett <abeburnett@gmail.com>
2014-09-24 15:18:03 -0700

    create comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+    redirect_to @post
+  end
+
 end
